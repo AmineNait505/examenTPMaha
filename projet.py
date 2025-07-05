e2bae2e2507eabdd0aa8bff39e3c88b305c55562
Nait Slimene Amine <137489572+AmineNait505@users.noreply.github.com>
2025-07-05 13:29:05 +0100

    Modifying 3

--- projet.py
+++ projet.py
@@ -8,3 +8,11 @@ def somme(t):
 Data=[1,3,5]
 som=sum(Data)
 print("La somme des éléments de la liste est:", som)
+
+if not Data:
+    print("La liste est vide.")
+else:
+    print("le min de la liste est:", min(Data))
+    print("le max de la liste est:", max(Data))
+    print("La somme des éléments de la liste est:", sum(Data))
+
